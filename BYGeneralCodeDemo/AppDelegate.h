d27357f0a95df103afef8dea541ff22bc8191abd
宝音 <baoyin.sby@120yibao.com>
2017-04-17 17:22:59 +0800

    Initial Commit

---
+++ BYGeneralCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BYGeneralCodeDemo
+//
+//  Created by BaoYin on 2017/4/17.
+//  Copyright © 2017年 BaoYin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
